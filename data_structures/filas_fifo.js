65ca7f0c52a5c6ebc7aa77abc9d8407a1b0f4f70
ctrindadedev <caiomedtrindade@gmail.com>
2025-01-30 17:40:08 -0300

    more js

--- data_structures/filas_fifo.js
+++ data_structures/filas_fifo.js
@@ -46,7 +46,66 @@ restaurante.enqueue("Gutzz")
 restaurante.dequeue()
 restaurante.dequeue()
     // 2. Crie uma fila que armazene músicas em uma playlist e remova a música quando for tocada.
+    class playlistSpot extends FilaMercado{
+      constructor(){
+        super()
+        this.playlist = []
+      }
+    adicionarMusica(nome){
+      this.playlist.push(nome)
+      console.log(`Música ${nome} adicionado com sucesso`)
+    }
+    tocarMusica(){
+      if (this.isEmpty === true){
+        console.log("A playlist está vazia. Adicione uma música na fila")
+      }
+      console.log(`Tocando a música ${this.playlist[0]}`)
+      this.playlist.shift()
+    }
+    isEmpty(){
+this.playlist.length() === 0;
+    }
+    }
+    const Sequencia = new playlistSpot
+    Sequencia.adicionarMusica("90210")
+    Sequencia.adicionarMusica("4x4")
+    Sequencia.adicionarMusica("AT THE PARTY")
+    Sequencia.tocarMusica()
+    Sequencia.tocarMusica()
 
     // 4. Use uma fila para implementar um sistema de envio de mensagens em lote.
 
     // 5. Crie uma fila que armazene processos pendentes e os execute na ordem de chegada.
+    class Processos extends FilaMercado{
+      constructor(){
+        super()
+        this.processos = []
+      }
+      isEmpty(){
+        this.processos.length() === 0
+      }
+      novoProcesso(nome){
+        this.processos.push(nome)
+        console.log(`O processo ${nome} foi adicionado na fila`)
+      }
+      executarProcesso(){
+        if (this.isEmpty === true){
+          console.log(`Todos processos já foram executados`)
+        }
+        else{
+          console.log(`Executando o processo ${this.processos[0]}`)
+          this.processos.shift()
+          if (this.isEmpty === false){
+          let tamanho = this.processos.length()
+          console.log(`Ainda faltam ${tamanho} processo(s) para serem executados.`)
+        }
+      }
+    }
+    }
+    const papelada = new Processos
+papelada.novoProcesso("Pog")
+papelada.novoProcesso("Pog1")
+papelada.novoProcesso("Pog2")
+for (let i = 0; i < 3; i++) {
+papelada.executarProcesso()
+}
