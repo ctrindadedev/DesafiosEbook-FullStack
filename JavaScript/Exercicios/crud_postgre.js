c6f305ab05ae992dbb061612349d78462a6bb6ae
ctrindadedev <caiomedtrindade@gmail.com>
2025-02-27 17:47:52 -0300

    crud and databases + html/css

---
+++ JavaScript/Exercicios/crud_postgre.js
@@ -0,0 +1,24 @@
+//Registro dos scrips simples  usados para o seguinte desafio de CRUD básico
+
+//Create (Adicione 3 Usuários na Tabela)
+//insert into usuarios(email, idade)
+//values('messi@gmail.com', 50),
+//('bola@gmail.com', 40),
+//('cr7@gmail.com', 40)
+
+//Read (Consulte os dados da tabela)
+//Sem filtro
+// select * from usuarios
+//Com filtro (Idade menor da 30)
+//select * from usuarios where idade < 30
+
+//Update
+//Altere o email de um usuário especifico
+//update usuarios set email = 'sapato@gmail.com' where email = 'chuteira@gmail.com'
+//Altere a idade de todos os usuários para 40
+//update usuarios set idade = 40
+//Delete
+//Remova um registro usando o `id`
+//delete from usuarios where id = 2
+//Exclua todos os registros da tabela, mas mantenha a estrutura da tabela.
+//delete from usuarios
---
+++ JavaScript/Exercicios/data_base_postgre.js
@@ -0,0 +1,25 @@
+//Exercicios Relacionados a desenvolver scripts sql e entender banco de dados relacionais
+
+// create database empresa
+
+// --
+
+// create table funcionarios(ID serial primary key ,
+// nome varchar(100) not null,
+// cargo varchar(100) not null,
+// salario float not null);
+
+// --
+
+// alter table funcionarios add data_admissao date
+
+// --
+
+// insert into funcionarios(nome, cargo, salario, data_admissao)
+// values('pedro', 'analista de sistemas', '900', '2025/01/07'),
+// ('Alex', 'Desenvolvedor FullStack', '5000', '2024/12/31')
+// ('caio', 'estágiario', '700', '2024/12/31')
+
+// --
+
+// select * from funcionarios
