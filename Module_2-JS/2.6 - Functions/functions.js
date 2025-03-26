da27544a36273dc91435984296211374f55e2ce9
ctrindadedev <caiomedtrindade@gmail.com>
2025-03-25 22:44:09 -0300

    cond/func

---
+++ Module_2-JS/2.6 - Functions/functions.js
@@ -0,0 +1,12 @@
+// Crie um arquivo chamado calculo_area.js e declare uma função chamada
+// calculaArea que aceite o comprimento e a largura de um retângulo e retorne a
+// área. 📏
+// 2. Crie uma função chamada parOuImpar que aceite um número e exiba "Par" se
+// o número for par e "Ímpar" se o número for ímpar. 🔢
+// 3. Crie uma função anônima que aceite dois números e retorne a soma deles.
+// Atribua essa função a uma variável chamada soma e chame-a com diferentes
+// valores. ➕
+// 4. Escreva uma arrow function que aceite um nome e exiba uma mensagem de
+// boas-vindas personalizada. 🚀
+// 5. Crie uma função chamada converterCelsiusParaFahrenheit que aceite uma
+// temperatura em Celsius e retorne a temperatura convertida para Fahrenheit
